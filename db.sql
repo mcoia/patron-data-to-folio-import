c9c29045d8f11850b619968f917eb66c005152a7
Scott <angelwilliamscott@gmail.com>
2024-02-02 14:30:09 -0600

    .

--- db.sql
+++ db.sql
@@ -54,41 +54,7 @@ create table stage_patron
 
 create table if not exists patron
 (
-    ID                     SERIAL primary key,
-    job_id                 int,
-    externalID             varchar,
-    active                 bool,
-    username               varchar,
-    patronGroup            varchar,
-    cluster                varchar,
-    institution            varchar,
-    field_code             varchar,
-    patron_type            varchar,
-    pcode1                 varchar,
-    pcode2                 varchar,
-    pcode3                 varchar,
-    home_library           varchar,
-    patron_message_code    varchar,
-    patron_block_code      varchar,
-    patron_expiration_date varchar,
-    name                   varchar,
-    firstname              varchar,
-    middlename             varchar,
-    lastname               varchar,
-    address                varchar,
-    street                 varchar,
-    city                   varchar,
-    state                  varchar,
-    zip                    varchar,
-    telephone              varchar,
-    address2               varchar,
-    telephone2             varchar,
-    department             varchar,
-    unique_id              varchar,
-    barcode                varchar,
-    email_address          varchar,
-    note                   varchar,
-    file                   varchar
+    ID SERIAL primary key
 );
 
 create table if not exists patron_address
@@ -106,4 +72,12 @@ create table if not exists patron_phone
     ID           SERIAL primary key,
     patron_id    int,
     phone_number varchar
+);
+
+
+create table if not exists parser_modules
+(
+    ID          SERIAL primary key,
+    institution varchar,
+    module      varchar
 );
