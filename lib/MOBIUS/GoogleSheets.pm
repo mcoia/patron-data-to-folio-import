709125c0b2c2990f758d26b24c1a1cf221da8f58
Scott Angel <angelwilliamscott@gmail.com>
2024-03-17 11:22:36 -0500

    .

--- lib/MOBIUS/GoogleSheets.pm
+++ lib/MOBIUS/GoogleSheets.pm
@@ -1,15 +1,20 @@
-package MOBIUS::GoogleSheets;
+package MOBIUS::GoogleSheet;
 use strict;
 use warnings FATAL => 'all';
+use Data::Dumper;
+use Cwd qw();
+
+my $cwd = Cwd::cwd();
 
 sub new
 {
     my $class = shift;
     my $self = {
-        'url'     => shift,
-        'zipURL'  => '',
-        'sheetID' => '',
-        'gid'     => ''
+        'url'              => shift,
+        'zipURL'           => '',
+        'sheetID'          => '',
+        'gid'              => '',
+        'tmpDirectoryPath' => $cwd . '/tmp',
     };
     bless $self, $class;
     return $self;
@@ -32,18 +37,24 @@ sub getSheetCSVByID
 
 sub getSheets
 {
+
     my $self = shift;
+
     $self->_parseURL();
-    $self->downloadZip();
-    $self->extractZipFile();
+
+    # Download the google sheet as a zip file
+    # $self->_mkdir("$self->{tmpDirectoryPath}/$self->{sheetID}")->_downloadZip()->_extractZipFile()->_deleteZipFile();
+
+    $self->_loadHTMLFiles($self->_getSheetsHTMLFilePaths())->_parseHTML2CSV();
+
 }
 
 sub _parseURL
 {
+
     my $self = shift;
 
     my $url = $self->{'url'};
-
     my $sheetID = ($url =~ /https:\/\/docs\.google\.com\/spreadsheets\/d\/(.*)\//)[0];
     my $gid = ($url =~ /gid=(.*)$/)[0];
     $url =~ s/edit#gid=\d*/export\?format=zip/g;
@@ -56,33 +67,150 @@ sub _parseURL
     $self->{'sheetID'} = $sheetID;
     $self->{'gid'} = $gid;
 
-
 }
 
-sub downloadZip
+sub _downloadZip
 {
+
     my $self = shift;
+
     my $url = $self->{'zipURL'};
     print "downloading zip... $url\n";
-    my $fileName = $self->{sheetID};
-    my @zip = `wget -q -O $fileName.zip $url`;
-
-}
-
-sub extractZipFile
-{
-    my $self = shift;
-    print "do work\n";
+    my @zip = `wget -q -O $self->{tmpDirectoryPath}/$self->{sheetID}.zip $url`;
 
     return $self;
 }
 
-sub setURL
+sub _extractZipFile
 {
+    my $self = shift;
+
+    my @unzip = `unzip -o $self->{tmpDirectoryPath}/$self->{sheetID}.zip -d $self->{tmpDirectoryPath}/$self->{sheetID}`;
+
+    return $self;
+}
+
+sub _setURL
+{
+
     my $self = shift;
     my $url = shift;
     $self->{url} = $url;
     return $self;
+}
+
+sub _setTempDirectory
+{
+
+    my $self = shift;
+    my $filePath = shift;
+
+    $self->{tmpDirectoryPath} = $filePath;
+
+    return $self;
+
+}
+
+sub _mkdir
+{
+    my $self = shift;
+    my $filePath = shift;
+
+    my @command = `mkdir -p $filePath`;
+
+    return $self;
+}
+
+sub _deleteZipFile
+{
+    my $self = shift;
+    my @deleteZip = `rm $self->{tmpDirectoryPath}/$self->{sheetID}.zip`;
+    return $self;
+}
+
+sub _getSheetsHTMLFilePaths
+{
+
+    my $self = shift;
+
+    my @htmlFiles = `ls $self->{tmpDirectoryPath}/$self->{sheetID}/*.html`;
+    chomp(@htmlFiles);
+
+    return \@htmlFiles;
+
+}
+
+sub _loadHTMLFiles
+{
+
+    my $self = shift;
+    my $files = shift;
+
+    my $index = 0;
+    for my $file (@{$files})
+    {
+        my ($fileName) = $file =~ /$self->{tmpDirectoryPath}\/$self->{sheetID}\/(.*.html)$/g;
+        $self->{sheets}->[$index]->{raw} = $self->_openFile($file);
+        $self->{sheets}->[$index]->{name} = $fileName;
+        $index++;
+    }
+
+    return $self;
+
+}
+
+sub _openFile
+{
+    my $self = shift;
+    my $file = shift;
+
+    # // open the file $file
+    open(my $fh, '<', $file) or die "Could not open file '$file' $!";
+    my $html = do {
+        local $/;
+        <$fh>
+    };
+    close($fh);
+
+    return $html;
+
+}
+
+sub _parseHTML2CSV
+{
+    my $self = shift;
+
+    for my $sheet (@{$self->{sheets}})
+    {
+
+        my $html = $sheet->{raw};
+        ($html) = $html =~ /(<table.*table>)/g;
+
+        my $tableColumns = $self->getTableColumns($html);
+
+
+        # $html =~ s/(\/\w*)>/$1>\n/g;
+        # $html =~ s/></>\n</g;
+        # my @tags = $html =~ /(.*)\n/g;
+
+        # print "$html\n";
+        # print "@th\n";
+
+    }
+
+}
+
+sub getTableColumns
+{
+    my $self = shift;
+    my $html = shift;
+
+    my @th = $html =~ /(<thead.*\/thead>)/g;
+
+    my @data =
+
+    print "@data\n";
+
 }
 
 1;
--- t/testGoogleSheets.pl
+++ t/testGoogleSheets.pl
@@ -5,11 +5,7 @@ use lib qw(../lib);
 use MOBIUS::GoogleSheets;
 use Data::Dumper;
 
-my $url = "https://docs.google.com/spreadsheets/d/1MaQlncNOAMpT_-mWUfd650f88Vv1xFOZKw7pIfqr5qE/edit#gid=1585277753";
-
-# my $google = MOBIUS::GoogleSheets->new($url);
-my $google = MOBIUS::GoogleSheets->new();
-
-my $sheets = $google->setURL($url)->getSheets();
-# my $sheets = $google->getSheets();
-
+# my $url = "https://docs.google.com/spreadsheets/d/1MaQlncNOAMpT_-mWUfd650f88Vv1xFOZKw7pIfqr5qE/edit#gid=1585277753";
+my $url = "https://docs.google.com/spreadsheets/d/1kAdsJ9Hk9iW6cW9S9uAiosutIo_eBtFE7zRQy8iczbY/edit#gid=0";
+my $google = MOBIUS::GoogleSheet->new($url);
+my $sheets = $google->getSheets();
